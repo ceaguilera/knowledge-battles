94181c47e43a4d8b39f0a745079fee57c9fcd3dc
Carlos Aguilera <ceaf.21@gmail.com>
2017-04-23 22:23:22 -0400

    +Add function for key management

--- public/game/game.controller.js
+++ public/game/game.controller.js
@@ -1,5 +1,30 @@
 angular.module('knowledgeBattlesApp.game')
 .controller('game', function($scope, $rootScope, $log, $http, auth, $cookies, gridService, gameService) {
-    
+        
+    window.focus();
+    window.onkeydown = keypress
+
+    function keypress() {
+        switch (event.keyCode) {
+            case 37:
+                gameService.move("left", $scope.grids);
+                console.log(37);
+                break;
+            case 38:
+                gameService.move("up", $scope.grids);
+                console.log(38);
+                break;
+            case 39:
+                gameService.move("rigth", $scope.grids);
+                console.log(39);
+                break;
+            case 40:
+                gameService.move("down", $scope.grids);
+                console.log(40);
+                break;
+            default:
+                break;
+        }
+    }
 
 });
