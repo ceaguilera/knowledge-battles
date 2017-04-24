9b07ba9c5ff463de48724b4d4c2ff1e4d7d60e54
Carlos Aguilera <ceaf.21@gmail.com>
2017-04-23 22:30:35 -0400

    +Added function that creates the board

--- public/services/grid.service.js
+++ public/services/grid.service.js
@@ -1,4 +1,12 @@
 angular.module('knowledgeBattlesApp').
     service("gridService", function($rootScope, $http) {
-
+        /* Create table 10x10 */
+        this.createGrid = function() {
+            let grid = new Array(10);
+            for(let i=0;i<10;i++){
+                grid[i] = new Array(10);
+            }
+            return grid;
+        }
+    
 });
