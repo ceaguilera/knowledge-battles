97224632ef0ab3c7e58d7a01c41f471c905de7cf
Carlos Aguilera <ceaf.21@gmail.com>
2017-04-24 23:48:23 -0400

    %add behavior modals

--- public/game/game.controller.js
+++ public/game/game.controller.js
@@ -8,7 +8,7 @@ angular.module('knowledgeBattlesApp.game')
         $rootScope.posYAct = null;
         gridService.initGrid($scope.grids);
         gridService.loadEnemy();
-        gridService.assignEnemies(1, $scope.grids);
+        gridService.assignEnemies(6, $scope.grids);
         gridService.potitionInitial($scope.grids);
         //console.log($scope.grids, $rootScope.posXAct, $rootScope.posYAct);
     })();
@@ -21,25 +21,25 @@ angular.module('knowledgeBattlesApp.game')
         switch (event.keyCode) {
             case 37:
                 gameService.move("left", $scope.grids);
-                //gameService.enemyOn($scope.grids);
+                gameService.enemyOn($scope.grids);
                 gameService.mapCompleted($scope.grids);
                 //console.log(37);
                 break;
             case 38:
                 gameService.move("up", $scope.grids);
-                //gameService.enemyOn($scope.grids)
+                gameService.enemyOn($scope.grids)
                 gameService.mapCompleted($scope.grids);
                 //console.log(38);
                 break;
             case 39:
                 gameService.move("rigth", $scope.grids);
-                //gameService.enemyOn($scope.grids);
+                gameService.enemyOn($scope.grids);
                 gameService.mapCompleted($scope.grids);
                 //console.log(39);
                 break;
             case 40:
                 gameService.move("down", $scope.grids);
-                //gameService.enemyOn($scope.grids);
+                gameService.enemyOn($scope.grids);
                 gameService.mapCompleted($scope.grids);
                 //console.log(40);
                 break;
