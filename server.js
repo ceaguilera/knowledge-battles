ece0c99edfe5284a7dc4128f4361ecb8ad94ccb5
Carlos Aguilera <ceaf.21@gmail.com>
2017-04-25 12:40:30 -0400

    %clear code

--- server.js
+++ server.js
@@ -3,20 +3,15 @@ var path    = require("path");
 
 var app = express();
 
-
+/* Asing port 8080 */
 app.set('port', (process.env.PORT || 8080))
 app.listen(app.get('port'));
 
+/* File static */
 app.use(express.static(path.join(__dirname, 'public/')));
 
+/* All petition send index */
 app.get('*', (req, res) => {
   res.type('html');
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
-
-
-//app.use(express.static('public'));
-//assuming app is express Object.
-//app.get('/',function(req,res){    
-//     res.sendFile(path.join(__dirname+'/public/app/index.html'));
-//});
